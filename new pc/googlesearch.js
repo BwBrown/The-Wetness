cda8c8e3309d7f5ea3a67a3da4ffbdaa0c903f90
Brandon Brown <8964286+BwBrown@users.noreply.github.com>
2020-11-16 02:41:00 -0500

    0.03
    1. Navbar on Home Page no longer pushes all items on page down (need to do the same for the pc page)
    2. Navbar slightly optimized for better styling
    3. Added links and icons for websites featured
    4. Added google search
    5. Removed some unused div containers
    6. Some images are now properly called/indexed allowing for local hosting

---
+++ new pc/googlesearch.js
@@ -0,0 +1,5 @@
+<form action="https://www.google.com/search" class="searchform" method="get" name="searchform" target="_blank">
+<input name="sitesearch" type="hidden" value="example.com">
+<input autocomplete="on" class="form-control search" name="q" placeholder="Search in example.com" required="required"  type="text">
+<button class="button" type="submit">Search</button>
+</form>
